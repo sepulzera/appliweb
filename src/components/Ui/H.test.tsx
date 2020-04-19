eca058d2621a173e8c0ab0792a0ffcc05fb9e43f
sepulzera <hartung_spam@gmx.de>
2020-04-19 20:19:48 +0200

    Update tests

--- src/components/Ui/H.test.tsx
+++ src/components/Ui/H.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import H from './H';
 
-test('renders title', () => {
-  const { getByText } = render(<App />);
+test('renders heading', () => {
+  const { getByText } = render(<H variant='h1'>This is your new app!</H>);
   const titleElement = getByText(/This is your new app!/i);
   expect(titleElement).toBeInTheDocument();
 });
