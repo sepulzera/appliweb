91ed6218fe38874f2977bf3b6a1e2e15c484e201
sepulzera <hartung_spam@gmx.de>
2020-11-19 20:12:15 +0100

    #44 TimelineHeading component

---
+++ src/components/Timeline/TimelineHeading.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { AnyComponent } from '../../types/Types';
+import CapsHeading from '../Heading/CapsHeading';
+
+/** {@link TimelineHeading} Props. */
+export interface ITimelineHeadingProps {
+  /** Heading text. */
+  children:   AnyComponent;
+}
+
+const useStyles = makeStyles(theme => ({
+  timelineHeading: {
+    paddingTop: '0.5em',
+    paddingLeft: '1.2em',
+    borderTop: `0.1rem solid ${theme.palette.text.secondary}`,
+
+    marginBottom: '0.5em',
+  },
+}));
+
+/**
+ * Heading for a {@link Timeline}.
+ *
+ * @param props - {@link ITimelineHeadingProps}.
+ */
+const TimelineHeading: React.FC<ITimelineHeadingProps> = (props: ITimelineHeadingProps) => {
+  const classes = useStyles();
+
+  return (
+    <CapsHeading className={classes.timelineHeading}>
+      {props.children}
+    </CapsHeading>
+  );
+};
+
+export default TimelineHeading;
